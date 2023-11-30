9937991c71ddcf317ca4603bd12dabd81aa0f136
Mnafairy <mnafairy@gmail.com>
2023-11-30 13:32:31 +0800

    third commit

--- function.js
+++ function.js
@@ -90,6 +90,153 @@ console.log(result);
 //!= tentsehgui baih yum bol
 //>= tentsuu buyu ers ih
 //<=
-// && 2 zuil asuuhad hereglen gehdee hoyulaa unen baival true 
+// && 2 zuil asuuhad hereglen gehdee hoyulaa unen baival true
 //! NOT if its true -false  false-true
 // || return  "or"
+
+//ugugldiin turul primitive bolon non primitive gej 2 angilj bga
+//primitive
+// string= ''
+let counter = false;
+let string = 11;
+console.log(typeof string); //"boolean"
+// undefined utga onooj ogoogui baina
+// null -hooson
+
+// function definition
+// function(parameter1,parameter2)
+
+//non-primitive
+//object type    let person = {}
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 12,
+  email: "@gmail.com",
+  address: "",
+};
+console.log(person.email);
+
+//array indexeer damjuulj handana
+let array = ['car', 'apple', 'pen', ['orange']];
+console.log(array[0]); //car
+console.log(array[1]); //apple
+console.log(array[2]); //pen
+console.log(array[3][0]),array[0]l;
+
+//city gedeg nertei array zarl
+// city ni dotroo 8 hotiin ner aguulna
+// hot bolgoniig console.log ashiglasd hevlej haruulna
+let city=['Tokyo','New York',"Beijing",'Seoul','Sydney','Paris','Los Angeles','Wellington'];
+console.log(city[0]);
+console.log(city[1]);
+console.log(city[2]);
+console.log(city[3]);
+console.log(city[4]);
+console.log(city[5]);
+console.log(city[6]);
+console.log(city[7]);
+// usa object uusgene
+// state uudeer ni variable uusgene
+// state bolgonoor ni population number 
+// state dotroo object uusgene
+// object dotroo city name,city population
+let usa = {
+  alabama:{
+    capital:"Montgomery",
+    cityPopulation:200603,
+  },
+  alaska:{
+    capital:"Juneau",
+    cityPopulation:32255,
+  },
+  arizona:{
+    capital:"Phoenix",
+    cityPopulation:1608139,
+  },
+  arkansas:{
+    capital:"Little rock",
+    cityPopulation:204405,
+  },	
+  california:{
+    capital:"Sacramento",
+    cityPopulation:524943,
+  },
+  colorado:{
+    capital:"Denver",
+    cityPopulation:715522,
+  },
+  connecticut:{
+    capital:"Hartford",
+    cityPopulation:121054,
+  },
+  delaware:{
+    capital:"Dover",
+    cityPopulation:39403,
+  },
+  florida:{
+    capital:"Tallahassee",
+    cityPopulation:196169,
+  },
+  georgia:{
+    capital:"Atlanta",
+    cityPopulation:498715,
+  },
+  hawaii:{
+    capital:"Honolulu",
+    cityPopulation:350964,
+  },
+  idaho:{
+    capital:"Boise",
+    cityPopulation:235684,
+  },
+  illinois:{
+    capital:"Springfield",
+    cityPopulation:114394,
+  },
+  indiana:{
+    capital:"Indianapolis",
+    cityPopulation:887642,
+  },
+  iowa:{
+    capital:"Des Moines",
+    cityPopulation:214133,
+  },
+}
+console.log (usa.iowa);
+
+// salary ni 2 sayaas ih
+// niigmiin daatgaliin shimtgel toldog baih
+// passaporttoi esvel identity cardtai baih
+// bas in person bh esvel itegmjelheer baih
+// dept ni 1 sayas baga bwal zeel garna
+
+let loan={
+  salary:2000000,
+  socialInsurance:260000,
+  passport:false,
+  identityCard:true,
+  person:true,
+  credential:false,
+  dept:400000,
+}
+if (loan.salary>=2000000 && loan.socialInsurance>0 && (loan.passport || loan.identityCard) && (loan.person || loan.credential) && loan.dept<1000000){
+  console.log ("I GIFF U LOAN")
+} else console.log ("I DO NOT GIFF U LOAN") 
+
+// if its raining today, take umbrella
+// if its snowing today, take umbrella
+// if its too sunny today take umbrella
+
+// ogogodson toog 5 bolon 11t hubaagddag eshiig tootsoh function bich
+// too 5d huvaagdan gehdee 11th huvaagdahgui
+
+let todayForecast ={
+  raining:true
+  snowing:false
+  sunny:true
+}
+if (todayForecast.raining ||todayForecast.snowing){
+  console.log("Take umbrella")
+};
+
--- spoj/RGB7105.js
+++ spoj/RGB7105.js
@@ -1,7 +1,35 @@
 function sumNumbersGreaterThan80(a, b, c, d) {
-  if (a>=80)
-  let answer= a+b+c+d
-  return answer
-} 
+  let result = 0;
+
+  if (a > 80) result = result + a;
+  if (b > 80) result = result + b;
+  if (c > 80) result = result + c;
+  if (d > 80) result = result + d;
+  return result;
+}
 let answer = sumNumbersGreaterThan80(85, 75, 96, 69);
+console.log("Answer", answer);
+
+//5d huvaagddag toonuudin urjver
+function numbersDividedBy5Product(a, b, c, d) {
+  let product = 1;
+  if (a / 5) product = product * a;
+  if (b / 5) product = product * b;
+  if (c / 5) product = product * c;
+  if (d / 5) product = product * d;
+  return product;
+}
+let answer = numbersDividedBy5Product(25, 23, 25, 10);
+console.log("Answer", answer);
+
+// 80 aas ih ba tegsh esvel 3t huvaagddag niilber
+
+function moreThanEighty(a, b, c) {
+  let result =0;
+  if ((a>80)%2==0 || a%3==0) result=result+a;
+  if ((b>80)%2==0 || b%3==0) result=result+b;
+  if ((c>80)%2==0 || c%3==0) result=result+c;
+  return result;
+}
+let answer = moreThanEighty(93, 85, 120);
 console.log("Answer", answer);
