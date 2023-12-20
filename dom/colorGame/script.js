308f70d841c400c49e99aab33d4d45e9a1b2fc49
Оргилсайхан Чулуунболор <23LP8480@imac888.local>
2023-12-20 13:06:04 -0800

    dec20

--- dom/colorGame/script.js
+++ dom/colorGame/script.js
@@ -2,24 +2,31 @@ const root = document.getElementById("root");
 const startButton = document.createElement("button");
 startButton.innerText = "Start";
 root.appendChild(startButton);
-
+const container = document.createElement("div");
+root.appendChild(container);
+container.setAttribute("id", "container");
 startButton.addEventListener("click", startGame);
 
 function startGame() {
-  const box = document.createElement("div");
-  const diffBox = document.createElement("div");
   const red = Math.floor(Math.random() * 245) + 10;
   const green = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
-  const randomColor = `rgb(${red},${green},${blue})`;
-  const differentColor = `rgb(${red - 10},${green},${blue})`;
-
-  box.setAttribute("class", "box");
+  const rando = Math.floor(Math.random() * 9);
+  const diffBox = document.createElement("div");
+  const differentColor = `rgb(${red - 50},${green},${blue})`;
   diffBox.setAttribute("class", "box");
-  box.style.backgroundColor = randomColor;
   diffBox.style.backgroundColor = differentColor;
-  root.appendChild(box);
-  root.appendCHild(diffBox);
+  for (let i = 1; i < 9; i++) {
+      const box = document.createElement("div");
+      box.setAttribute("class", "box");
+      const randomColor = `rgb(${red},${green},${blue})`;
+      box.style.backgroundColor = randomColor;
+      container.appendChild(box);
+      container.appendChild(diffBox);
+    if (rando == i) {
+      box.style.backgroundColor = differentColor;
+    }
+  }
 }
 
 // localStorage.setItem("item", [1, 2, 3]);
@@ -31,3 +38,4 @@ function startGame() {
  * 1ungu ni ondoo baih yostoi busad ni adilhan ungiin
  * reset button darhad togloo reset hiigdene
  * amjilttai olson tohioldold shineer 3x3 generate
+ */
