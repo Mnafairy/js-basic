fcdbb3b0d96b1b7394c622bd2f996a79341dcc89
Orgil <mnafairy@gmail.com>
2023-12-25 19:55:52 +0800

    dec 25 8pm

--- dom/colorGame/script3.js
+++ dom/colorGame/script3.js
@@ -3,8 +3,9 @@ const header = document.createElement("div");
 header.setAttribute("class", "header");
 
 let levelC = 0;
+
 const level = document.createElement("p");
-level.innertext = "Level" + levelC;
+level.innerText = "Level " + levelC;
 
 const startButton = document.createElement("button");
 startButton.setAttribute("id", "startButton");
@@ -25,10 +26,56 @@ const boxes = document.createElement("div");
 boxes.setAttribute("class", "boxes");
 root.appendChild(header);
 root.appendChild(gameBody);
+
 startButton.addEventListener("click", startGame);
 
 function addBoxes() {
-  const red = Math.floor(Math.random() * 255);
-  const green = Math.floor(Math.random() * 255);
-  
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const randomColor = `rgb(${red},${green},${blue})`;
+  const random = Math.floor(Math.random() * 8);
+
+  for (i = 0; i < 9; i++) {
+    let box = document.createElement("div");
+    box.className = "box";
+    box.setAttribute("id", i);
+    if (i == random) {
+      box.style.backgroundColor = `rgb(${red - 50},${green},${blue})`;
+    } else {
+      box.style.backgroundColor = randomColor;
+    }
+    boxes.appendChild(box);
+
+    box.addEventListener("click", (e) => {
+      let boxId = e.target.id;
+      if (boxId == random) {
+        levelC++;
+        boxes.innerHTML = "";
+        level.innerText = "Level " + levelC;
+        if (levelC == 5) {
+          gameBody.innerHTML = "WIN";
+        } else {
+          addBoxes();
+        }
+      } else {
+        chanceC--;
+        if (chanceC == 0) {
+          gameBody.innerHTML = "LOSE";
+        }
+        chance.innerText = "Chance " + chanceC;
+      }
+    });
+  }
+  gameBody.appendChild(boxes);
+}
+
+function startGame() {
+  levelC = 0;
+  level.innerText = "Level " + levelC;
+  chanceC = 3;
+  chance.innerText = "Chance " + chanceC;
+  boxes.innerHTML = "";
+  gameBody.innerHTML = "";
+  addBoxes();
 }
